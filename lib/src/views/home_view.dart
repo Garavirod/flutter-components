23e83d0aa9fad335e9b94804c11a2d4948d490ba
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-04 17:20:03 -0500

    Utils function were created in order to work with icons

--- lib/src/views/home_view.dart
+++ lib/src/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:components/src/providers/menu_provider.dart';
+import 'package:components/src/utils/icon_utils.dart';
 import 'package:flutter/material.dart';
 
 
@@ -15,15 +16,36 @@ class HomeView extends StatelessWidget{
 
   /* Methods */
   Widget _createList(){
-    print(menuProvider.options);
-    return ListView(
-      children: this._createListItems(),
+    // Load data from provider    
+    // Fururebuilder solve something of type future and draw it
+    return FutureBuilder(
+      future: menuProvider.loadData(),
+      initialData: [],
+      builder: ( BuildContext context, AsyncSnapshot <List<dynamic>> snapshot){
+        /* Builder return a widget that works with data prevously porccesd */
+        return ListView(
+          children: this._createListItems( snapshot.data )
+        );
+      }
     );
+
+
+   
   }
 
-  List<Widget> _createListItems(){
-    return [
-      ListTile( title: Text('Help'),)
-    ];
+  List<Widget> _createListItems( List<dynamic> data ){
+    final List<Widget> opts = [];
+
+    data.forEach((opt) { 
+      final temp = ListTile(
+        title: Text( opt['texto'] ),
+        leading: getIcon(opt['icon']),
+        trailing: Icon( Icons.arrow_forward_ios_outlined, color: Colors.amber,),
+        onTap: (){},
+      );
+      opts..add(temp)
+          ..add(Divider());
+    });
+    return opts;
   }
 }
