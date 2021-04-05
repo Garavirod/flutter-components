d2715e8ae3d49f84e42111800e5d49111d99f8ed
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-05 15:59:46 -0500

    show alert component was created

--- lib/src/views/alerts_view.dart
+++ lib/src/views/alerts_view.dart
@@ -7,12 +7,52 @@ class AlertView extends StatelessWidget {
       appBar: AppBar(
         title: Text('Alerts'),
       ),
+      body: Center(
+        child: RaisedButton(
+          child: Text('Show an alert'),
+          color: Colors.amber,
+          textColor:Colors.white,
+          shape: StadiumBorder(),
+          onPressed: () => this._showalert(context),
+        ),        
+      ),
       floatingActionButton: FloatingActionButton(
         child: Icon( Icons.arrow_back),
         onPressed: (){
           Navigator.pop(context);
         },        
       ),
+    );
+  }
+
+  /* Methods */
+  void _showalert( BuildContext context ){
+    showDialog(
+      context: context,
+      barrierDismissible: true, 
+      builder: (context) { // what is going to draw it
+        return AlertDialog(
+          shape: RoundedRectangleBorder( borderRadius: BorderRadius.circular(10.0) ),
+          title: Text('Title'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min, // adtapd to min content
+            children: <Widget> [
+              Text('This is the contant od show alert!'),
+              FlutterLogo( size:100.0)
+            ],
+          ),
+          actions: [
+            FlatButton(
+              child: Text('Cancel'),
+              onPressed: () => Navigator.of(context).pop(), //close alert
+            ),
+            FlatButton(
+              child: Text('Acept'),
+              onPressed: (){},
+            )
+          ],
+        );
+      }
     );
   }
 }
