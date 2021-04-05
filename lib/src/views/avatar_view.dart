a2b28b94832ff0dbc4332a72b4f09ea35abbf986
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-05 16:18:44 -0500

    Avaters were added with properties

--- lib/src/views/avatar_view.dart
+++ lib/src/views/avatar_view.dart
@@ -1,3 +1,5 @@
+
+
 import 'package:flutter/material.dart';
 
 class AvatarView extends StatelessWidget {  
@@ -6,8 +8,29 @@ class AvatarView extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(5.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://media.gq.com.mx/photos/5ce19f41d09b9ac33d16885a/16:9/w_1920,c_limit/john%20wick%203.jpg'),
+              radius: 25.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 5.0),
+            child: CircleAvatar(
+              child: Text('RO'),
+              backgroundColor: Colors.amber,
+            ),
+          )
+        ],
       ),
-      
+      body: Center(
+        child: FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://imagenes.milenio.com/vO1Dy55CjCA-DI6g8aKKZ6MQ20E=/958x596/smart/https://www.milenio.com/uploads/media/2014/10/31/john-wick.jpeg'),
+        ),        
+      ),
     );
   }
 }
