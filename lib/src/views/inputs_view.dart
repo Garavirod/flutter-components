7cd98e71a77f2928b7d7e799b9fc9875adb00cad
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-07 13:57:18 -0500

    Password input and email input were added

--- lib/src/views/inputs_view.dart
+++ lib/src/views/inputs_view.dart
@@ -10,7 +10,8 @@ class InputView extends StatefulWidget {
 
 class _InputViewState extends State<InputView> {
 
-  String _name;
+  String _name  = "";
+  String _email = "";
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +26,10 @@ class _InputViewState extends State<InputView> {
         ),
         children: <Widget> [
           this.createInput(),
+          Divider(),
+          this._createsEmailInput(),
+          Divider(),
+          this._createPassword(),
           Divider(),
           this._createPerson(),
         ],
@@ -56,10 +61,53 @@ class _InputViewState extends State<InputView> {
     );
   }
 
-  /* Creates a person based on Input */
+  /* Creates a label for show the name written into input */
   Widget _createPerson(){
     return ListTile( 
       title: Text("Name ${this._name}"),
+      subtitle: Text("Email ${this._email}"),
+    );
+  }
+
+  /* Creates an input for email */
+  Widget _createsEmailInput(){
+    return TextField(
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),        
+        hintText: 'Set your email here',
+        labelText: 'Email',        
+        suffixIcon: Icon(Icons.email_outlined),
+        icon: Icon(Icons.email_sharp)
+      ),
+      onChanged: (currValue){
+        setState(() {
+          this._email = currValue;          
+        });
+      },
+    );
+  }
+
+  /* Creates an input type password */
+  Widget _createPassword(){
+    return TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0),
+        ),        
+        hintText: 'Set your password here',
+        labelText: 'Password',        
+        suffixIcon: Icon(Icons.lock),
+        icon: Icon(Icons.lock_open)
+      ),
+      onChanged: (currValue){
+        setState(() {
+          this._email = currValue;          
+        });
+      },
     );
   }
 }
