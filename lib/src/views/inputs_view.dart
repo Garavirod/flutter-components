905edb5a24886545043a3aa26cc6e9b50cd100bc
Garavirod <rodrigogarciaavila26@gmail.com>
2021-04-07 15:30:44 -0500

    Dropdwon input was added into view Inputs

--- lib/src/views/inputs_view.dart
+++ lib/src/views/inputs_view.dart
@@ -13,6 +13,8 @@ class _InputViewState extends State<InputView> {
   String _name  = "";
   String _email = "";
   String _date  = "";
+  String _choosenPower = "fly";
+  List<String> _superPowers = ['fly','speed','super strenght'];
   TextEditingController _inputFieldControlloer = new TextEditingController();
 
   @override
@@ -34,6 +36,8 @@ class _InputViewState extends State<InputView> {
           this._createPassword(),
           Divider(),
           this._createDateInput( context ),
+          Divider(),
+          this._createDropDown(),
           Divider(),
           this._createPerson(),
         ],
@@ -62,14 +66,6 @@ class _InputViewState extends State<InputView> {
           this._name = currValue;          
         });
       },
-    );
-  }
-
-  /* Creates a label for show the name written into input */
-  Widget _createPerson(){
-    return ListTile( 
-      title: Text("Name ${this._name}"),
-      subtitle: Text("Email ${this._email}"),
     );
   }
 
@@ -152,6 +148,49 @@ class _InputViewState extends State<InputView> {
         FocusScope.of(context).requestFocus(new FocusNode()); // remove focus of input
         this._selectDate(context);
       },
+    );
+  }
+
+
+  List<DropdownMenuItem<String>> getOptionDropDown(){
+    List<DropdownMenuItem<String>> listIems = [];
+    this._superPowers.forEach((power) {
+      listIems.add(DropdownMenuItem(
+        child: Text(power),
+        value: power,
+      ));
+    });
+    return listIems;
+  }
+
+  /* Creates a combobox */
+  Widget _createDropDown(){
+    return Row(
+      children: [
+        Icon(Icons.select_all),
+        SizedBox(width:30.0),
+        Expanded(
+            child: DropdownButton(
+            value: this._choosenPower,//current choosen value,
+            items: this.getOptionDropDown(),
+            onChanged: ( opt ){
+              setState(() {
+                this._choosenPower = opt;          
+              });
+            },
+          ),
+        ),
+      ],
+    );
+    
+  }
+
+  /* Creates a label for show the name written into input */
+  Widget _createPerson(){
+    return ListTile( 
+      title: Text("Name ${this._name}"),
+      subtitle: Text("Email ${this._email}"),
+      trailing: Text(this._choosenPower),
     );
   }
 }
